65270106a1a5534b5b6fdb51a0b5c7fd66c322ad
EIMAR QUINN <G00374109@gmit.ie>
2020-10-18 22:00:55 +0100

    Lab upload

---
+++ src/components/footer.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+
+ export class Footer extends React.Component{
+
+    render(){
+        return(
+            <div>
+                <h1>My Footer in another component</h1>
+            </div>
+        );
+    }
+}
